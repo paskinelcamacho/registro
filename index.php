f94b5a22913df33be1146ac83167a5143ef7e974
paskinelcamacho <paskycamacho@gmail.com>
2018-02-28 17:23:13 -0600

    primera prueba

---
+++ index.php
@@ -0,0 +1,23 @@
+<h2> Bienvenida </h2>
+<br/>
+<p> Misión<br/>
+
+Proporcionar la infraestructura y servicios de Tecnologías de Información y Comunicaciones que requiere la Facultad para su buen funcionamiento en el desarrollo de las actividades académico-administrativas de la misma. 
+<br/>
+Visión
+<br/>
+Ser un área de servicios que brinde de manera eficiente y oportuna soluciones de Tecnologías de Información y Comunicaciones que impulsen y favorezcan de manera continua el desarrollo e innovación tecnológica de la Facultad. 
+<br/>
+
+Valores
+<br/>
+El Centro de Informática ofrece todos los días sus servicios en el marco de los siguientes valores:
+<ul>
+    <li>Responsabilidad</li>
+    <li>Integridad y ética</li>
+    <li>Respeto</li>
+    <li>Actitud de servicio</li>
+    <li>Trabajo en equipo</li>
+    <li>Mejora continua</li>
+    <li>Compromiso social</li>
+</ul>
---
+++ registro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset="utf-8">
+ <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<h1>Registro de usuarios</h1>
+
+<div>
+<FORM ACTION="enviar_email.php" METHOD="post">
+<label class="etiquetas" for="nombre">Nombre</label> <input class="cajas" type="text" size=36 name="nombre"></input><br/>
+<label class="etiquetas" for="paterno">Apellido paterno</label> <input class="cajas" type="text" size=36 name="paterno"></input><br/>
+
+<label class="etiquetas" for="materno">Apellido materno</label> <input class="cajas" type="text" size=36 name="materno"></input><br/>
+
+<label class="etiquetas" for="telefono">teléfono</label> <input class="cajas" type="text" size=36 name="telefono"></input><br/>
+
+<label class="etiquetas" for="email">Correo electrónico</label> <input class="cajas" type="text" size=36 name="email"></input><br/>
+
+<INPUT type="submit" value="Enviar">
+</div>
+</FORM>
+</body>
+</html>
+
+
+
